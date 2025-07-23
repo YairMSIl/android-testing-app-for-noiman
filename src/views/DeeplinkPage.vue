8ce2847f216ff126e71623be82ae7e087b1008df
YairMS <yairms.il@gmail.com>
2025-07-23 15:15:30 +0300

    github pages test

---
+++ src/views/DeeplinkPage.vue
@@ -0,0 +1,17 @@
+<template>
+  <ion-page>
+    <ion-header>
+      <ion-toolbar>
+        <ion-title>Deep Link Page</ion-title>
+      </ion-toolbar>
+    </ion-header>
+    <ion-content class="ion-padding">
+      <h1>Welcome via Deep Link!</h1>
+      <p>You have successfully opened this page using a deep link.</p>
+    </ion-content>
+  </ion-page>
+</template>
+
+<script setup lang="ts">
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/vue';
+</script>
